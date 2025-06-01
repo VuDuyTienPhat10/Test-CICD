83096d540eda30ae5c0f77efcbfe935969057bdf
phatvdt <goldhand9999@gmail.com>
2025-06-01 16:52:27 +0700

    Add /test route to app.js for testing purposes

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello CI/CD 1/6/2025 4h48 : childrens day');
 });
 
+app.get('/test', (req, res) => {
+  res.send('Test 1/6/2025 4h50');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
