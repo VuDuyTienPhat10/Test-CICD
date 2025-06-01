f1c133751b46dfe398980589bdf88368fced02d5
phatvdt <goldhand9999@gmail.com>
2025-06-01 16:41:41 +0700

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD!');
+  res.send('Hello CI/CD 1/6/2025 : childrens day');
 });
 
 app.listen(PORT, () => {
